81e9816ca4abad62a753f8c4c7660a4d335bbd26
Matthew Johnson <90540643+mjohns91@users.noreply.github.com>
2026-06-22 17:13:50 -0400

    Update plugins/module_utils/args_common.py
    Co-authored-by: Mandar <mandkulk@redhat.com>

--- plugins/module_utils/args_common.py
+++ plugins/module_utils/args_common.py
@@ -14,6 +14,7 @@ AUTH_ARG_SPEC = {
     "url": {
         "required": True,
         "aliases": ["vault_address"],
+        "fallback": (env_fallback, ["VAULT_ADDR"])
     },
     "namespace": {
         "default": "admin",
